9e72ce85d2d8d564ac1f929acf065558023ed79b
Mona Lisa Mameli <lisa.noetzel@gmail.com>
2019-11-04 18:21:49 -0500

    connecting to Heroku

--- rate-admin-app/backend/routes/api/users.js
+++ rate-admin-app/backend/routes/api/users.js
@@ -20,25 +20,24 @@ router.post('/register', (req, res) => {
     if (!isValid) {
         return res.status(400).json(errors);
     }
-    user.findOne({ email: req.body.email }).then(user => {
-        if (user) {
+    user.findOne({ email: req.body.email }).then(DBuser => {
+        if (DBuser) {
             return res.status(400).json({ email: 'Email already exists' });
         } else {
-            const newUser = new user({
+            const newUser = {
                 name: req.body.name,
                 email: req.body.email,
-                username: req.body.username,
                 password: req.body.password,
-            });
+            };
             //Hashing password before saving in DB
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if (err) throw err;
                     newUser.password = hash;
-                    newUser
-                        .save()
+                    user
+                        .create(newUser)
                         .then(user => res.json(user))
-                        .catch(err => console.log(err));
+                        .catch(err => res.json(err)); //will give error in Postman in JSON format
                 });
             });
         }
